062caf0456d194772c3cd17c207ebc7a5cb57443
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-07-07 14:46:39 +0530

    feat: Add user data retrieval and display in DashboardCard component

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,6 @@
 import {Nav, NavLink} from "@/components/Nav";
 import React from "react";
+import {MdDashboard} from "react-icons/md";
 
 export default function AdminLayout({
                                         children,
@@ -8,7 +9,9 @@ export default function AdminLayout({
 }>) {
     return <>
         <Nav>
-            <NavLink href="/admin">Dashboard</NavLink>
+            <NavLink href="/admin">
+                <MdDashboard className="inline-block mr-1 mb-0.5" /> Dashboard
+            </NavLink>
             <NavLink href="/admin/products">Products</NavLink>
             <NavLink href="/admin/users">Customers</NavLink>
             <NavLink href="/admin/orders">Sales</NavLink>
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -20,13 +20,32 @@ async function getSalesData() {
     }
 }
 
+async function getUserData() {
+    const [userCount, orderData] = await Promise.all([
+        db.user.count(),
+        db.order.aggregate({
+            _sum: {pricePaidInCents: true}
+        })
+    ])
+
+    return {
+        userCount,
+        averageValuePerUser: userCount === 0 ? 0 : (orderData._sum.pricePaidInCents || 0) / (userCount * 100)
+    }
+}
+
 export default async function AdminDashboard() {
-    const salesData = await getSalesData()
+    const [salesData, userData] = await Promise.all([
+        getSalesData(),
+        getUserData()
+    ])
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <DashboardCard title="Sales" subtitle={formatNumber(salesData.numberOfSales)}
+            <DashboardCard title="Sales" subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
                            body={formatCurrency(salesData.amount)}/>
+            <DashboardCard title="Customers" subtitle={`${formatCurrency(userData.averageValuePerUser)} Average Value`}
+                           body={formatNumber(userData.userCount)}/>
         </div>
     )
 }
